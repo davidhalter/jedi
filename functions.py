8d1f3342a9ea3e8a2f9b97304ac64a06aa06a7c2
David Halter <davidhalter88@gmail.com>
2012-03-06 00:30:02 +0100

    started with the vim interface

---
+++ functions.py
@@ -0,0 +1,16 @@
+import pyfuzzyparser
+
+def complete(source, row, colum, file_callback=None):
+    """
+    An auto completer for python files.
+
+    :param source: The source code of the current file
+    :type source: string
+    :param row: The row to complete in.
+    :type row: int
+    :param col: The column to complete in.
+    :type col: int
+    :return: list
+    :rtype: list
+    """
+    return []
