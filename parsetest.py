21c489e90f22b74f6bea67de411cc55289509b04
David Halter <davidhalter88@gmail.com>
2012-04-12 01:08:16 +0200

    rename test to create a place for tests

--- parsetest.py
+++ parsetest.py
@@ -140,11 +140,11 @@ class c1():
 c, 1, c3()) [0].pop()
 
 c = u"asdf".join([1,2])
-
+matrx_test = [[1,2], [1,3]]
 c = c1().c3().sleep()
 asdf = c1; asdf2 = asdf
 b= asdf2
 #import parsing as test
 c = b().c3()
 1.0.fromhex(); import flask ; flsk = flask.Flask + flask.Request;
-c2(flask.
+matrix_test.
