4feb8f5a3aa5c9818f557c7a83fd3672563d29ed
David Halter <davidhalter88@gmail.com>
2012-04-21 19:35:24 +0200

    import tests

---
+++ test/completion/imports.py
@@ -0,0 +1,20 @@
+
+
+import sys
+#? ['prefix']
+sys.prefix
+
+#? ['append']
+sys.path.append
+
+
+# --- builtin math ---
+
+from math import *
+#? ['cos', 'cosh']
+cos
+
+import os
+
+#? ['dirname']
+os.path.dirname
