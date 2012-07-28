e9d9213f1049caced138c9d921f79da907c0cfd8
David Halter <davidhalter88@gmail.com>
2012-07-28 18:31:36 +0200

    goto tests, which i previously forgot to commit

---
+++ test/completion/goto.py
@@ -0,0 +1,69 @@
+# goto command test are a different in syntax
+
+a = 1
+
+#! []
+b
+#! ['a=1']
+a
+
+b = a
+c = b
+#! ['c=b']
+c
+
+#! ['module math']
+import math
+#! ['import math']
+math
+
+#! ['import math']
+b = math
+#! ['b=math']
+b
+
+class C(object):
+    def b(self):
+        #! ['b=math']
+        b
+        #! ['def b']
+        self.b
+        #! 14 ['def b']
+        self.b()
+        #! 11 ['class C']
+        self.b
+        return 1
+
+    #! ['def b']
+    b
+
+#! ['b=math']
+b
+
+#! ['def b']
+C.b
+#! ['def b']
+C().b
+#! 0 ['class C']
+C().b
+#! 0 ['class C']
+C().b
+
+D = C
+#! ['def b']
+D.b
+#! ['def b']
+D().b
+
+#! 0 ['D=C']
+D().b
+#! 0 ['D=C']
+D().b
+
+def c():
+    return ''
+
+#! ['def c']
+c
+#! 0 ['def c']
+c()
