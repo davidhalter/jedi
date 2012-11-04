6f8d4776794beb624057c273d14a3c8ab8f9ba2e
David Halter <davidhalter88@gmail.com>
2012-11-04 01:20:57 +0100

    sys.path hack to be a fully compliant py3k library

--- jedi/__init__.py
+++ jedi/__init__.py
@@ -1,2 +1,8 @@
+import sys
+
+# python imports are hell sometimes. Especially the combination of relative
+# imports and circular imports... Just avoid it:
+sys.path.insert(0, __path__[0])
+
 from .api import Script, NotFoundError, set_debug_function
 from . import settings
