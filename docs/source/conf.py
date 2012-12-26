6b3119937146823224e818fcb8641a936287ae5a
Danilo Bargen <gezuru@gmail.com>
2012-12-26 16:42:58 +0100

    Set author to "Jedi contributors"

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Jedi'
-copyright = u'2012, David Halter'
+copyright = u'2012, Jedi contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -185,7 +185,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'Jedi.tex', u'Jedi Documentation',
-   u'David Halter', 'manual'),
+   u'Jedi contributors', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -215,7 +215,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'jedi', u'Jedi Documentation',
-     [u'David Halter'], 1)
+     [u'Jedi contributors'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -229,7 +229,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'Jedi', u'Jedi Documentation',
-   u'David Halter', 'Jedi', 'One line description of project.',
+   u'Jedi contributors', 'Jedi', 'One line description of project.',
    'Miscellaneous'),
 ]
 
