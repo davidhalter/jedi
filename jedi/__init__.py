464685f433c7a29368a49d26b97ada54c4b39a51
David Halter <davidhalter88@gmail.com>
2012-12-27 19:07:49 +0100

    move docsring of api.py to __init__.py

--- jedi/__init__.py
+++ jedi/__init__.py
@@ -1,3 +1,29 @@
+"""
+Jedi is an autocompletion library for Python. It offers additonal
+services such as goto / get_definition / pydoc support /
+get_in_function_call / related names.
+
+To give you a simple exmple how you can use the jedi library,
+here is an exmple for the autocompletion feature:
+
+>>> import jedi
+>>> source = '''import json; json.l'''
+>>> script = jedi.Script(source, 1, 19, '')
+>>> script
+<jedi.api.Script at 0x7f6d40f3db90>
+>>> completions = script.complete()
+>>> completions
+[<Completion: load>, <Completion: loads>]
+>>> completions[0].complete
+'oad'
+>>> completions[0].word
+'load'
+
+As you see Jedi is pretty simple and allows you to concentrate
+writing a good text editor, while still having very good IDE features
+for Python.
+"""
+
 import sys
 
 # python imports are hell sometimes. Especially the combination of relative
@@ -8,8 +34,6 @@ from .api import Script, NotFoundError, set_debug_function
 from . import settings
 
 from . import api
-
-__doc__ = api.__doc__
 
 del api
 
--- jedi/api.py
+++ jedi/api.py
@@ -1,27 +1,5 @@
 """
-Jedi is an autocompletion library for Python. It offers additonal
-services such as goto / get_definition / pydoc support /
-get_in_function_call / related names.
-
-To give you a simple exmple how you can use the jedi library,
-here is an exmple for the autocompletion feature:
-
->>> import jedi
->>> source = '''import json; json.l'''
->>> script = jedi.Script(source, 1, 19, '')
->>> script
-<jedi.api.Script at 0x7f6d40f3db90>
->>> completions = script.complete()
->>> completions
-[<Completion: load>, <Completion: loads>]
->>> completions[0].complete
-'oad'
->>> completions[0].word
-'load'
-
-As you see Jedi is pretty simple and allows you to concentrate
-writing a good text editor, while still having very good IDE features
-for Python.
+TODO doc
 """
 from __future__ import with_statement
 __all__ = ['Script', 'NotFoundError', 'set_debug_function']
