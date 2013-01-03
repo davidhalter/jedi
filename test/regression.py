e692f87258db03e494571f06eb0b816291caebc7
Danilo Bargen <gezuru@gmail.com>
2013-01-03 22:55:37 +0100

    Implemented __eq__ and __ne__ for Completion object

--- test/regression.py
+++ test/regression.py
@@ -336,6 +336,20 @@ class TestFeature(Base):
         any_re"""
         self.assertEqual(self.get_def(s)[0].full_name, 're.RegexObject')
 
+    def test_equality_operators(self):
+        """Test the equality operator overloading for Completion objecs."""
+        src1 = '''import json; json.l'''
+        src2 = '''import json; json.d'''
+        c1 = self.complete(src1)
+        c2 = self.complete(src1)
+        c3 = self.complete(src2)
+        self.assertTrue(c1 == c1)
+        self.assertFalse(c1 != c1)
+        self.assertTrue(c1 == c2)
+        self.assertFalse(c1 != c2)
+        self.assertTrue(c1 != c3)
+        self.assertFalse(c1 == c3)
+
 
 class TestSpeed(Base):
     def _check_speed(time_per_run, number=4, run_warm=True):
