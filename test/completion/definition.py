5f49fb8c7c0f9e7a05d4f9b730d7f3e872229d60
Takafumi Arakaki <aka.tkf@gmail.com>
2013-03-10 08:01:38 +0100

    Add blackbox tests using column number

--- test/completion/definition.py
+++ test/completion/definition.py
@@ -15,3 +15,23 @@ isinstance(None,
 #? isinstance
 isinstance(None, 
 )
+
+# Note: len('isinstance(') == 11
+#? 11 isinstance
+isinstance()
+
+# Note: len('isinstance(None,') == 16
+##? 16 isinstance
+isinstance(None,)
+
+# Note: len('isinstance(None,') == 16
+##? 16 isinstance
+isinstance(None, )
+
+# Note: len('isinstance(None, ') == 17
+##? 17 isinstance
+isinstance(None, )
+
+# Note: len('isinstance( ') == 12
+##? 12 isinstance
+isinstance( )
