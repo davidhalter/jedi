31df9a92fe2f541fc58ff3278ed95594fff850ed
Takafumi Arakaki <aka.tkf@gmail.com>
2013-03-10 07:34:37 +0100

    Add test/completion/definition.py

---
+++ test/completion/definition.py
@@ -0,0 +1,17 @@
+"""
+Fallback to callee definition when definition not found.
+- https://github.com/davidhalter/jedi/issues/131
+- https://github.com/davidhalter/jedi/pull/149
+"""
+
+#? isinstance
+isinstance(
+)
+
+#? isinstance
+isinstance(None,
+)
+
+#? isinstance
+isinstance(None, 
+)
