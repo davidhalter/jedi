956ad502766eddbaf3c81672a30e58c814ba8437
Takafumi Arakaki <aka.tkf@gmail.com>
2013-03-15 14:10:24 +0100

    Make more examples in make_definitions

--- test/test_api_classes.py
+++ test/test_api_classes.py
@@ -1,10 +1,17 @@
+import textwrap
+
 import pytest
 
 from jedi import api
 
 
 def make_definitions():
-    return api.defined_names("""
+    """
+    Return a list of definitions for parametrized tests.
+
+    :rtype: [jedi.api_classes.BaseDefinition]
+    """
+    source = textwrap.dedent("""
     import sys
 
     class C:
@@ -15,6 +22,20 @@ def make_definitions():
     def f():
         pass
     """)
+
+    definitions = []
+    definitions += api.defined_names(source)
+
+    source += textwrap.dedent("""
+    variable = sys or C or x or f""")
+    lines = source.splitlines()
+    script = api.Script(source, len(lines), len('variable'), None)
+    definitions += script.definition()
+
+    script2 = api.Script(source, 4, len('class C'), None)
+    definitions += script2.related_names()
+
+    return definitions
 
 
 @pytest.mark.parametrize('definition', make_definitions())
