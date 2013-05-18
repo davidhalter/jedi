ee5f96f1195e59fbf23b9b4d35bdffd79f4aa613
Takafumi Arakaki <aka.tkf@gmail.com>
2013-05-18 23:25:54 +0200

    Fix test failure
    The bug was introduced when I merged branch 'interpreter-api' at
    5f2477d5bf1b8601246e246adada21ab6c28c268.
    This patch redo (part of) 2846fe980b468ac43b1c09461a192486787664aa.

--- test/test_regression.py
+++ test/test_regression.py
@@ -453,27 +453,6 @@ class TestFeature(TestBase):
 
         cache.parser_cache = temp_cache
 
-    def test_quick_completion(self):
-        sources = [
-            ('import json; json.l', (1, 19)),
-            ('import json; json.l   ', (1, 19)),
-            ('import json\njson.l', (2, 6)),
-            ('import json\njson.l  ', (2, 6)),
-            ('import json\njson.l\n\n', (2, 6)),
-            ('import json\njson.l  \n\n', (2, 6)),
-            ('import json\njson.l  \n  \n\n', (2, 6)),
-        ]
-        for source, pos in sources:
-            # Run quick_complete
-            quick_completions = api._quick_complete(source)
-            # Run real completion
-            script = jedi.Script(source, pos[0], pos[1], '')
-            real_completions = script.completions()
-            # Compare results
-            quick_values = [(c.full_name, c.line, c.column) for c in quick_completions]
-            real_values = [(c.full_name, c.line, c.column) for c in real_completions]
-            self.assertEqual(quick_values, real_values)
-
 
 class TestGetDefinitions(TestBase):
 
