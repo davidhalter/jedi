3d4a71f6bb84fe4e5c7f51b109a55a7560ebb673
Laurens Van Houtven <_@lvh.cc>
2013-06-23 22:50:38 +0200

    Use Parser.module instead of Parser.scope

--- test/test_absolute_import.py
+++ test/test_absolute_import.py
@@ -8,7 +8,7 @@ def test_explicit_absolute_imports():
     Detect modules with ``from __future__ import absolute_import``.
     """
     parser = Parser("from __future__ import absolute_import", "test.py")
-    assert parser.scope.explicit_absolute_import
+    assert parser.module.explicit_absolute_import
 
 
 def test_no_explicit_absolute_imports():
@@ -16,7 +16,7 @@ def test_no_explicit_absolute_imports():
      Detect modules without ``from __future__ import absolute_import``.
     """
     parser = Parser("1", "test.py")
-    assert not parser.scope.explicit_absolute_import
+    assert not parser.module.explicit_absolute_import
 
 
 def test_dont_break_imports_without_namespaces():
@@ -26,7 +26,7 @@ def test_dont_break_imports_without_namespaces():
     """
     src = "from __future__ import absolute_import\nimport xyzzy"
     parser = Parser(src, "test.py")
-    assert parser.scope.explicit_absolute_import
+    assert parser.module.explicit_absolute_import
 
 
 @base.cwd_at("test/absolute_import")
