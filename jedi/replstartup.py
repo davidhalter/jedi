c615e70889e05f05ac8e363399449a6018d9d095
David Halter <davidhalter88@gmail.com>
2013-07-31 17:30:53 +0200

    only use repl keyword for now in __main__.py.

--- jedi/replstartup.py
+++ jedi/replstartup.py
@@ -4,7 +4,7 @@
 To use Jedi completion in Python interpreter, add the following in your shell
 setup (e.g., ``.bashrc``)::
 
-    export PYTHONSTARTUP="$(python -m jedi)"
+    export PYTHONSTARTUP="$(python -m jedi repl)"
 
 Then you will be able to use Jedi completer in your Python interpreter::
 
