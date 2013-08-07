6ed33d7c8d33bd715dba7c9dff0d2a143890c1f3
David Halter <davidhalter88@gmail.com>
2013-08-07 17:00:46 +0430

    remove interpreter test from test_regression

--- test/test_regression.py
+++ test/test_regression.py
@@ -439,43 +439,6 @@ class TestSpeed(TestBase):
         #print(jedi.imports.imports_processed)
 
 
-class TestInterpreterAPI(unittest.TestCase):
-    def check_interpreter_complete(self, source, namespace, completions,
-                                   **kwds):
-        script = api.Interpreter(source, [namespace], **kwds)
-        cs = script.complete()
-        actual = [c.word for c in cs]
-        self.assertEqual(sorted(actual), sorted(completions))
-
-    def test_complete_raw_function(self):
-        from os.path import join
-        self.check_interpreter_complete('join().up',
-                                        locals(),
-                                        ['upper'])
-
-    def test_complete_raw_function_different_name(self):
-        from os.path import join as pjoin
-        self.check_interpreter_complete('pjoin().up',
-                                        locals(),
-                                        ['upper'])
-
-    def test_complete_raw_module(self):
-        import os
-        self.check_interpreter_complete('os.path.join().up',
-                                        locals(),
-                                        ['upper'])
-
-    def test_complete_raw_instance(self):
-        import datetime
-        dt = datetime.datetime(2013, 1, 1)
-        completions = ['time', 'timetz', 'timetuple']
-        if is_py33:
-            completions += ['timestamp']
-        self.check_interpreter_complete('(dt - dt).ti',
-                                        locals(),
-                                        completions)
-
-
 def test_settings_module():
     """
     jedi.settings and jedi.cache.settings must be the same module.
