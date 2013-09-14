92237b559877e3b7114096759bc7e3ed17efc6dc
David Halter <davidhalter88@gmail.com>
2013-09-14 23:13:35 +0430

    test fix in python 3.2

--- test/test_imports.py
+++ test/test_imports.py
@@ -5,7 +5,7 @@ import pytest
 from jedi._compatibility import find_module_py33
 
 
-@pytest.mark.skipif('sys.version_info <= (3,2)')
+@pytest.mark.skipif('sys.version_info < (3,3)')
 def test_find_module_py33():
     """Needs to work like the old find_module."""
     print(find_module_py33('_io'))
