b1409c8f7489db6a6bdacc6f3f7cd8b4777fe4f9
Dave Halter <davidhalter88@gmail.com>
2014-01-10 02:03:02 +0100

    fix a name glitch in keywords

--- jedi/keywords.py
+++ jedi/keywords.py
@@ -2,7 +2,6 @@ import pydoc
 import keyword
 
 from jedi._compatibility import is_py3k
-from jedi.parser import representation as pr
 from jedi import common
 from jedi.evaluate import compiled
 
@@ -30,13 +29,23 @@ def keyword_names(*args, **kwargs):
     kwds = []
     for k in keywords(*args, **kwargs):
         start = k.start_pos
-        end = start[0], start[1] + len(k.name)
-        kwds.append(pr.Name(k.parent, [(k.name, start)], start, end, k))
+        kwds.append(KeywordName(k, k.name, start))
     return kwds
 
 
 def get_operator(string, pos):
     return Keyword(string, pos)
+
+
+class KeywordName(object):
+    def __init__(self, parent, name, start_pos):
+        self.parent = parent
+        self.names = [name]
+        self.start_pos = start_pos
+
+    @property
+    def end_pos(self):
+        return self.start_pos[0], self.start_pos[1] + len(self.name)
 
 
 class Keyword(object):
