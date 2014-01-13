8bf898524777a6252da27b1544616bcd5176c677
Dave Halter <davidhalter88@gmail.com>
2014-01-14 00:08:25 +0100

    move replstartup to the api

--- jedi/__main__.py
+++ jedi/__main__.py
@@ -5,4 +5,4 @@ from os.path import join, dirname, abspath
 if len(argv) == 2 and argv[1] == 'repl':
     # don't want to use __main__ only for repl yet, maybe we want to use it for
     # something else. So just use the keyword ``repl`` for now.
-    print(join(dirname(abspath(__file__)), 'replstartup.py'))
+    print(join(dirname(abspath(__file__)), 'api', 'replstartup.py'))
