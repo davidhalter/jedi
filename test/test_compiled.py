da27ce4d7caac77bc4f05957fea67e93a86ce336
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-02-26 13:52:46 +0100

    Execute test only on 64bit platforms

--- test/test_compiled.py
+++ test/test_compiled.py
@@ -21,13 +21,13 @@ def test_completions():
 
 @cwd_at('test/extensions')
 def test_call_signatures():
-    # if platform.architecture()[0] == '64bit':
-    package_name = "compiled%s%s" % sys.version_info[:2]
-    sys.path.insert(0, os.getcwd())
-    if os.path.exists(package_name):
-        s = jedi.Script("from %s import compiled; compiled.Foo(" %
-                        package_name)
-        defs = s.call_signatures()
-        for call_def in defs:
-            for param in call_def.params:
-                pass
+    if platform.architecture()[0] == '64bit':
+        package_name = "compiled%s%s" % sys.version_info[:2]
+        sys.path.insert(0, os.getcwd())
+        if os.path.exists(package_name):
+            s = jedi.Script("from %s import compiled; compiled.Foo(" %
+                            package_name)
+            defs = s.call_signatures()
+            for call_def in defs:
+                for param in call_def.params:
+                    pass
