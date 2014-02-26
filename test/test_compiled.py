2ee60675f1ce9604076a1ce087323894bbc80b5a
ColinDuquesnoy <colin.duquesnoy@gmail.com>
2014-02-26 13:51:40 +0100

    Add a test call_signatures for compiled modules
    Should fail

--- test/test_compiled.py
+++ test/test_compiled.py
@@ -10,10 +10,24 @@ from .helpers import cwd_at
 
 
 @cwd_at('test/extensions')
-def test_compiled():
+def test_completions():
     if platform.architecture()[0] == '64bit':
         package_name = "compiled%s%s" % sys.version_info[:2]
         sys.path.insert(0, os.getcwd())
         if os.path.exists(package_name):
             s = jedi.Script("from %s import compiled; compiled." % package_name)
             assert len(s.completions()) >= 2
+
+
+@cwd_at('test/extensions')
+def test_call_signatures():
+    # if platform.architecture()[0] == '64bit':
+    package_name = "compiled%s%s" % sys.version_info[:2]
+    sys.path.insert(0, os.getcwd())
+    if os.path.exists(package_name):
+        s = jedi.Script("from %s import compiled; compiled.Foo(" %
+                        package_name)
+        defs = s.call_signatures()
+        for call_def in defs:
+            for param in call_def.params:
+                pass
