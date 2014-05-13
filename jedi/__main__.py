5e2bb0ef9b17dd82e5d43796907797d583956b29
Dave Halter <davidhalter88@gmail.com>
2014-05-13 16:29:39 +0200

    Using python -m jedi force <path> you can use the linter for now.

--- jedi/__main__.py
+++ jedi/__main__.py
@@ -6,3 +6,12 @@ if len(argv) == 2 and argv[1] == 'repl':
     # don't want to use __main__ only for repl yet, maybe we want to use it for
     # something else. So just use the keyword ``repl`` for now.
     print(join(dirname(abspath(__file__)), 'api', 'replstartup.py'))
+elif len(argv) > 1 and argv[1] == 'force':
+    import jedi
+    import sys
+
+    if '--debug' in sys.argv:
+        jedi.set_debug_function()
+
+    for error in jedi.Script(path=sys.argv[2])._analysis():
+        print(error)
