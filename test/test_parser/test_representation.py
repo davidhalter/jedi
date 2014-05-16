06bae0e83508d97d1a4ff4c3ec9c18b606f5d50c
Dave Halter <davidhalter88@gmail.com>
2014-05-16 18:03:36 +0200

    fix test issue with python 2.7

--- test/test_parser/test_representation.py
+++ test/test_parser/test_representation.py
@@ -1,7 +1,8 @@
 from jedi.parser import Parser
+from jedi._compatibility import u
 
 
 def test_import_is_nested():
-    imp = Parser('import ').module.imports[0]
+    imp = Parser(u('import ')).module.imports[0]
     # should not raise an error, even if it's not a complete import
     assert not imp.is_nested()
