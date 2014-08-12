cf32e15f657e79bd9e1560e1a85ee41f8a6620c0
Dave Halter <davidhalter88@gmail.com>
2014-08-12 18:14:03 +0200

    Remove the old 'is not' logic to detect if not instances and use them to do branch predictions. This is not necessary anymore, since we now support that in a more general way (flow_analysis).

--- test/completion/basic.py
+++ test/completion/basic.py
@@ -357,16 +357,6 @@ foo = \
 foo
 
 # -----------------
-# if `is not` checks
-# -----------------
-
-foo = ['a']
-if foo is not None:
-    foo = ''.join(foo)
-#? str()
-foo
-
-# -----------------
 # module attributes
 # -----------------
 
