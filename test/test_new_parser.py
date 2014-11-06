00454daf57f2d24ca6c8080b6c3bb57dfcf32be2
Dave Halter <davidhalter88@gmail.com>
2014-11-06 14:16:17 +0100

    change the new parser tests

--- test/test_new_parser.py
+++ test/test_new_parser.py
@@ -1,22 +1,10 @@
-import logging
-
-from jedi.parser import pytree
-from jedi.parser.pgen2 import Driver
+from jedi.parser import Parser
 
 
 def test_basic_parsing():
     def compare(string):
         """Generates the AST object and then regenerates the code."""
-        assert d.parse_string(string).get_code() == string
-
-    #if self.options["print_function"]:
-    #    python_grammar = pygram.python_grammar_no_print_statement
-    #else:
-    # When this is True, the refactor*() methods will call write_file() for
-    # files processed even if they were not changed during refactoring. If
-    # and only if the refactor method's write parameter was True.
-    logger = logging.getLogger("RefactoringTool")
-    d = Driver(pytree.python_grammar, convert=pytree.convert, logger=logger)
+        assert Parser(string).module.get_code() == string
 
     compare('\na #pass\n')
     compare('wblabla* 1\t\n')
