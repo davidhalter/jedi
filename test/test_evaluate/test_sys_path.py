528b325c397b1e56a928b2098fec84b09e0ee770
Dave Halter <davidhalter88@gmail.com>
2014-12-07 14:41:57 +0100

    Remove precedence tests. They are not needed anymore, since precedence is now handled by the parser itself.

--- test/test_evaluate/test_sys_path.py
+++ test/test_evaluate/test_sys_path.py
@@ -1,12 +1,13 @@
 from jedi._compatibility import unicode
-from jedi.parser import Parser
+from jedi.parser import Parser, load_grammar
 from jedi.evaluate import sys_path, Evaluator
 
 
 def test_paths_from_assignment():
     def paths(src):
-        stmt = Parser(unicode(src)).module.statements[0]
-        return list(sys_path._paths_from_assignment(Evaluator(), stmt))
+        grammar = load_grammar()
+        stmt = Parser(grammar, unicode(src)).module.statements[0]
+        return list(sys_path._paths_from_assignment(Evaluator(grammar), stmt))
 
     assert paths('sys.path[0:0] = ["a"]') == ['a']
     assert paths('sys.path = ["b", 1, x + 3, y, "c"]') == ['b', 'c']
