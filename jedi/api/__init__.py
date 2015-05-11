258d5aee4a8733b8aedcc12e6acafa1104b2796e
Sid Shanker <sshanker220@gmail.com>
2015-05-10 18:59:55 -0700

    Added support for correct continue & break autocompletion.

--- jedi/api/__init__.py
+++ jedi/api/__init__.py
@@ -157,7 +157,7 @@ class Script(object):
                     if unfinished_dotted:
                         return completion_names
                     else:
-                        return keywords.keyword_names('import')
+                        return set([keywords.keyword('import').name])
 
             if isinstance(user_stmt, tree.Import):
                 module = self._parser.module()
@@ -168,7 +168,11 @@ class Script(object):
             if names is None and not isinstance(user_stmt, tree.Import):
                 if not path and not dot:
                     # add keywords
-                    completion_names += keywords.keyword_names(all=True)
+                    completion_names += keywords.completion_names(
+                        self._evaluator,
+                        user_stmt,
+                        self._pos,
+                        module)
                     # TODO delete? We should search for valid parser
                     # transformations.
                 completion_names += self._simple_complete(path, dot, like)
--- test/completion/keywords.py
+++ test/completion/keywords.py
@@ -4,3 +4,24 @@ raise
 
 #? ['except', 'Exception']
 except
+
+#? []
+b + continu
+
+#? []
+b + continue
+
+#? ['continue']
+b; continue
+
+#? ['continue']
+b; continu
+
+#? []
+c + brea
+
+#? []
+a + break
+
+#? ['break']
+b; break
