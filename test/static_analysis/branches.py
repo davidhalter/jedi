5d9fff50afc69d2b24e5163081ba809828bb3438
Dave Halter <davidhalter88@gmail.com>
2015-06-23 18:04:36 +0200

    Static analysis tests for type errors with variables.

--- test/static_analysis/branches.py
+++ test/static_analysis/branches.py
@@ -4,8 +4,22 @@ if random.choice([0, 1]):
     x = ''
 else:
     x = 1
-
-if x != 1:
-    x = x.upper()
+if random.choice([0, 1]):
+    y = ''
 else:
+    y = 1
+
+# A simple test
+if x != 1:
+    x.upper()
+else:
+    #! 2 attribute-error
+    x.upper()
     pass
+
+# This operation is wrong, because the types could be different.
+#! 6 type-error-operation
+z = x + y
+# However, here we have correct types.
+if type(x) == type(y):
+    z = x + y
