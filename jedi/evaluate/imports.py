d3fe5c830042bcdc4e4436c3a87ad464b57e71ad
Daniel Hahler <git@thequod.de>
2016-06-27 19:16:09 +0200

    module_path needs to be initialized for the None check
    Fixes https://github.com/davidhalter/jedi-vim/issues/587.

--- jedi/evaluate/imports.py
+++ jedi/evaluate/imports.py
@@ -304,6 +304,7 @@ class Importer(object):
                 return []
             else:
                 debug.dbg('search_module %s in paths %s', module_name, paths)
+                module_path = None
                 for path in paths:
                     # At the moment we are only using one path. So this is
                     # not important to be correct.
