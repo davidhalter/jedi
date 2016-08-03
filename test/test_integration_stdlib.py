c1bef454f5822c3fcc6c9781e94ba06540639406
Dave Halter <davidhalter88@gmail.com>
2016-08-03 09:21:51 +0200

    Restructure namedtuple tests a bit.

--- test/test_integration_stdlib.py
+++ test/test_integration_stdlib.py
@@ -2,6 +2,8 @@
 Tests of various stdlib related things that could not be tested
 with "Black Box Tests".
 """
+from textwrap import dedent
+
 import pytest
 from jedi import Script
 from jedi._compatibility import is_py26
@@ -16,10 +18,11 @@ pytestmark = pytest.mark.skipif('sys.version_info[0] < 3')
     ('s', ['smart']),
 ])
 def test_namedtuple_str(letter, expected):
-    source = "import collections\n" + \
-             "Person = collections.namedtuple('Person', 'name smart')\n" + \
-             "dave = Person('Dave', False)\n" + \
-             "dave.%s" % letter
+    source = dedent("""\
+        import collections
+        Person = collections.namedtuple('Person', 'name smart')
+        dave = Person('Dave', False)
+        dave.%s""") % letter
     result = Script(source).completions()
     completions = set(r.name for r in result)
     if is_py26:
@@ -29,10 +32,11 @@ def test_namedtuple_str(letter, expected):
 
 
 def test_namedtuple_list():
-    source = "import collections\n" + \
-             "Cat = collections.namedtuple('Person', ['legs', u'length', 'large'])\n" + \
-             "garfield = Cat(4, '85cm', True)\n" + \
-             "garfield.l"
+    source = dedent("""\
+        import collections
+        Cat = collections.namedtuple('Person', ['legs', u'length', 'large'])
+        garfield = Cat(4, '85cm', True)
+        garfield.l""")
     result = Script(source).completions()
     completions = set(r.name for r in result)
     if is_py26:
