24a23029a2e36d1fe1cc4be78f5d7b95e3ba5cd8
joncrall <crallj@rpi.edu>
2016-11-06 20:57:26 -0500

    Fixed another python 2.6 issue

--- test/test_evaluate/test_docstring.py
+++ test/test_evaluate/test_docstring.py
@@ -305,8 +305,8 @@ class TestDocstring(unittest.TestCase):
         assert len(y_type_list) == 2
         y_base_objs = set([t.base.obj for t in y_type_list])
         x_base_objs = set([t.base.obj for t in x_type_list])
-        assert x_base_objs == {builtins.int, builtins.str, builtins.list}
-        assert y_base_objs == {builtins.int, builtins.str}
+        assert x_base_objs == set([builtins.int, builtins.str, builtins.list])
+        assert y_base_objs == set([builtins.int, builtins.str])
 
     def test_google_find_return_types(self):
         from jedi.evaluate import docstrings
