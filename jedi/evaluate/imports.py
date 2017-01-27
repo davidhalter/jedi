0e8b3718eb489443d80f50f5d2cc277a296784fe
Cam Cope <cam@dropbox.com>
2017-01-27 15:45:05 -0800

    Fix uninitialized variable reference
    If the length of paths is 0, module_path was undefined.

--- jedi/evaluate/imports.py
+++ jedi/evaluate/imports.py
@@ -295,6 +295,7 @@ class Importer(object):
                 return set()
             else:
                 debug.dbg('search_module %s in paths %s', module_name, paths)
+                module_path = None
                 for path in paths:
                     # At the moment we are only using one path. So this is
                     # not important to be correct.
