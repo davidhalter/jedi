8542047e5c92f1430bc11ec462e8ffd24e103a1d
Dave Halter <davidhalter88@gmail.com>
2017-04-06 18:21:20 +0200

    Adding a tag should be part of the deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -eu -o pipefail
+
+# Create tag
+git tag $(python -c 'import jedi; print(jedi.__version__)')
+git push --tags
+
+# Package and upload to PyPI
 rm -rf dist/
 python setup.py sdist bdist_wheel
 # Maybe do a pip install twine before.
