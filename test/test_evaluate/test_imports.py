9fb7fb66da2f8eef536f41ae2c122af89921e5ec
Dave Halter <davidhalter88@gmail.com>
2017-05-07 16:39:32 +0200

    Move another test to delete a file.

--- test/test_evaluate/test_imports.py
+++ test/test_evaluate/test_imports.py
@@ -209,3 +209,11 @@ def test_after_from():
 
     check('from os\\\n', ['import'])
     check('from os \\\n', ['import'])
+
+
+def test_path_issues():
+    """
+    See pull request #684 for details.
+    """
+    source = '''from datetime import '''
+    assert jedi.Script(source).completions()
