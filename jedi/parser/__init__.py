f92e675400c8f5589cb9b84a3f037532a606ddbd
Dave Halter <davidhalter88@gmail.com>
2017-05-19 10:26:24 -0400

    Remove the whole parser.

--- jedi/parser/__init__.py
+++
@@ -1,8 +0,0 @@
-from jedi.parser.parser import ParserSyntaxError
-from jedi.parser.pgen2.pgen import generate_grammar
-from jedi.parser import python
-
-
-def parse(grammar, code):
-    raise NotImplementedError
-    Parser(grammar, code)
