9a43c35a4de0e01566de66a699c4a01880c0af66
Yariv Kenan <yariv.kenan@gmail.com>
2017-08-08 23:31:17 +0300

    fix `old_files` method
    Returns old files instead of new files info.
    Probably not what it was meant for.

--- jedi/refactoring.py
+++ jedi/refactoring.py
@@ -29,7 +29,7 @@ class Refactoring(object):
     def old_files(self):
         dct = {}
         for old_path, (new_path, old_l, new_l) in self.change_dct.items():
-            dct[new_path] = '\n'.join(new_l)
+            dct[old_path] = '\n'.join(old_l)
         return dct
 
     def new_files(self):
