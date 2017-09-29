3c2221ec2db014245d82397d07261071d1182dfe
Dave Halter <davidhalter88@gmail.com>
2017-09-29 15:47:36 +0200

    Don't use a star import.

--- jedi/evaluate/context/__init__.py
+++ jedi/evaluate/context/__init__.py
@@ -1,3 +1,5 @@
-from jedi.evaluate.context.base import *
+from jedi.evaluate.context.base import Context, iterate_contexts, \
+    TreeContext, ContextualizedName, ContextualizedNode, ContextSet, \
+    NO_CONTEXTS, iterator_to_context_set
 from jedi.evaluate.context.lazy import AbstractLazyContext, LazyKnownContext, \
     LazyKnownContexts, LazyTreeContext, LazyUnknownContext, get_merged_lazy_context
