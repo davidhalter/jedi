9d094b68f370a413a6cb319737dcadd06aada1af
Dave Halter <davidhalter88@gmail.com>
2017-12-14 22:23:59 +0100

    Cache the subprocess results

--- jedi/evaluate/compiled/subprocess/__init__.py
+++ jedi/evaluate/compiled/subprocess/__init__.py
@@ -275,6 +275,18 @@ class AccessHandle(object):
 
         #print >> sys.stderr, name
         #print('getattr', name, file=sys.stderr)
-        def compiled_method(*args, **kwargs):
+        return partial(self._workaround, name)
+
+    def _workaround(self, name, *args, **kwargs):
+        """
+        TODO Currently we're passing slice objects around. This should not
+        happen. They are also the only unhashable objects that we're passing
+        around.
+        """
+        if args and isinstance(args[0], slice):
             return self._subprocess.get_compiled_method_return(self.id, name, *args, **kwargs)
-        return compiled_method
+        return self._cached_results(name, *args, **kwargs)
+
+    @memoize_method
+    def _cached_results(self, name, *args, **kwargs):
+        return self._subprocess.get_compiled_method_return(self.id, name, *args, **kwargs)
