39a456be41d146692bbc5650caca668c7f0c48f1
Dave Halter <davidhalter88@gmail.com>
2017-12-30 23:08:32 +0100

    Experiment with travis and installing packages differently

---
+++ travis_install.sh
@@ -0,0 +1,10 @@
+set -e
+echo "ENV" $JEDI_TEST_ENVIRONMENT
+PYTHON=python-3.3
+
+# Check if the desired Python version already exists.
+$PYTHON --version && exit 0 || true
+
+# Otherwise download and install.
+wget https://s3.amazonaws.com/travis-python-archives/binaries/ubuntu/14.04/x86_64/$PYTHON.tar.bz2
+sudo tar xjf $PYTHON.tar.bz2 --directory /
