98a3da674c2b6bdd7ea8a64a93f4a5ba9e0b24cd
Dave Halter <davidhalter88@gmail.com>
2018-01-20 22:00:41 +0100

    Ahhh another bug... A bit stupid of me not to run the tests

--- test/test_evaluate/test_precedence.py
+++ test/test_evaluate/test_precedence.py
@@ -13,5 +13,5 @@ import pytest
 def test_equals(source):
     script = Script(source)
     node = script._module_node.children[0]
-    first, = script._get_module.eval_node(node)
+    first, = script._get_module().eval_node(node)
     assert isinstance(first, CompiledObject) and first.obj is True
