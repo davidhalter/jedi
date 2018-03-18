305fd66e1c8ebc348ae756ac4ce3a45b3526adb1
Dave Halter <davidhalter88@gmail.com>
2018-03-19 00:05:04 +0100

    Upgrade the wx widgets paths

--- scripts/wx_check.py
+++ scripts/wx_check.py
@@ -8,7 +8,7 @@ like a typical big Python modules. A mix between a lot of different Python
 things.
 
 You can view a markup version of it here:
-http://svn.wxwidgets.org/viewvc/wx/wxPython/trunk/src/gtk/_core.py?view=markup
+https://github.com/wxWidgets/wxPython/blob/master/src/gtk/_core.py
 """
 
 import resource
@@ -34,7 +34,8 @@ def process_memory():
     """
     return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
-uri = 'http://svn.wxwidgets.org/viewvc/wx/wxPython/trunk/src/gtk/_core.py?revision=74740&content-type=text%2Fplain&view=co'
+
+uri = 'https://raw.githubusercontent.com/wxWidgets/wxPython/master/src/gtk/_core.py'
 
 wx_core = urllib2.urlopen(uri).read()
 
