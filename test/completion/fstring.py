27655db8a97e616c563c5aab5b5171c3b3706a42
Dave Halter <davidhalter88@gmail.com>
2018-03-31 17:07:47 +0200

    With the changes in parso, f-strings are now completable

---
+++ test/completion/fstring.py
@@ -0,0 +1,17 @@
+# python >= 3.6
+
+class Foo:
+    bar = 1
+
+#? 10 int()
+f'{Foo.bar}'
+#? 10 ['bar']
+f'{Foo.bar}'
+#? 10 int()
+Fr'{Foo.bar'
+#? 10 ['bar']
+Fr'{Foo.bar'
+#? int()
+Fr'{Foo.bar
+#? ['bar']
+Fr'{Foo.bar
