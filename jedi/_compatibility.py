fabd8d53fbaa55ec0884a53773ecde4bb4e2220a
micbou <contact@micbou.com>
2018-06-20 14:34:39 +0200

    Use cPickle on Python 2 if available
    Attempt to load the C version of pickle on Python 2 as it is way faster.

--- jedi/_compatibility.py
+++ jedi/_compatibility.py
@@ -381,8 +381,12 @@ if is_py3:
 else:
     import Queue as queue
 
-
-import pickle
+try:
+    # Attempt to load the C implementation of pickle on Python 2 as it is way
+    # faster.
+    import cPickle as pickle
+except ImportError:
+    import pickle
 if sys.version_info[:2] == (3, 3):
     """
     Monkeypatch the unpickler in Python 3.3. This is needed, because the
