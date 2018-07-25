6bcac44050da3c2717bc958d7c94086453394498
Dave Halter <davidhalter88@gmail.com>
2018-07-25 11:44:48 +0200

    Add another stub file test

--- test/test_plugin/test_stub.py
+++ test/test_plugin/test_stub.py
@@ -24,3 +24,12 @@ def test_get_typeshed_directories():
     dirs = get_dirs(PythonVersionInfo(3, 6))
     assert dirs == {'stdlib/2and3', 'stdlib/3', 'stdlib/3.5', 'stdlib/3.6',
                     'third_party/2and3', 'third_party/3', 'third_party/3.5', 'third_party/3.6'}
+
+
+def test_get_stub_files():
+    def get_map(version_info):
+        return typeshed._create_stub_map(version_info)
+
+    base = os.path.join(typeshed._TYPESHED_PATH, 'stdlib', '3')
+    map_ = typeshed._create_stub_map(base)
+    assert map_['functools'] == os.path.join(base, 'functools.pyi')
