bc5ca4d8aee3e7fcab54710b6935ecdffe02c0ed
Dave Halter <davidhalter88@gmail.com>
2018-08-10 00:37:36 +0200

    Fix flask issues with unicode in Python2

--- jedi/plugins/flask.py
+++ jedi/plugins/flask.py
@@ -13,16 +13,16 @@ class FlaskPlugin(BasePlugin):
         def wrapper(evaluator, import_names, module_context, sys_path):
             if len(import_names) == 3 and import_names[:2] == ('flask', 'ext'):
                 # New style.
-                ipath = ('flask_' + import_names[2]),
+                ipath = (u'flask_' + import_names[2]),
                 try:
                     return callback(evaluator, ipath, None, sys_path)
                 except JediImportError:
-                    context_set = callback(evaluator, ('flaskext',), None, sys_path)
+                    context_set = callback(evaluator, (u'flaskext',), None, sys_path)
                     # If context_set has no content a JediImportError is raised
                     # which should be caught anyway by the caller.
                     return callback(
                         evaluator,
-                        ('flaskext', import_names[2]),
+                        (u'flaskext', import_names[2]),
                         next(iter(context_set)),
                         sys_path
                     )
