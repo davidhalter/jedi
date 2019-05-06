ea6462daf408285fe615b5a90bcfeb4614d59f48
Dave Halter <davidhalter88@gmail.com>
2019-05-06 19:50:26 +0200

    Forgot to add evaluate/names.py earlier

---
+++ jedi/evaluate/names.py
@@ -0,0 +1,39 @@
+from jedi.evaluate.filters import AbstractNameDefinition
+
+
+class ImportName(AbstractNameDefinition):
+    start_pos = (1, 0)
+    _level = 0
+
+    def __init__(self, parent_context, string_name):
+        self.parent_context = parent_context
+        self.string_name = string_name
+
+    def infer(self):
+        from jedi.evaluate.imports import Importer
+        return Importer(
+            self.parent_context.evaluator,
+            [self.string_name],
+            self.parent_context,
+            level=self._level,
+        ).follow()
+
+    def goto(self):
+        return [m.name for m in self.infer()]
+
+    def get_root_context(self):
+        # Not sure if this is correct.
+        return self.parent_context.get_root_context()
+
+    @property
+    def api_type(self):
+        return 'module'
+
+
+class SubModuleName(ImportName):
+    _level = 1
+
+
+class OsPathName(ImportName):
+    def infer(self):
+        return self.parent_context.evaluator.import_module(('os', 'path'))
