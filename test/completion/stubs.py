4f64dd30f95fd6f4d9d18fa4a06f99fa6a0df90d
Dave Halter <davidhalter88@gmail.com>
2019-05-15 22:23:23 +0200

    Make sure Python is still loadable in stub only folders

--- test/completion/stubs.py
+++ test/completion/stubs.py
@@ -1,6 +1,8 @@
 from stub_folder import with_stub, stub_only, with_stub_folder, stub_only_folder
 
+# -------------------------
 # Just files
+# -------------------------
 
 #? int()
 stub_only.in_stub_only
@@ -12,7 +14,9 @@ with_stub.in_with_stub_python
 with_stub.in_with_stub_stub
 
 
+# -------------------------
 # Folders
+# -------------------------
 
 #? int()
 stub_only_folder.in_stub_only_folder
@@ -23,9 +27,29 @@ with_stub_folder.in_with_stub_python_folder
 #? float()
 with_stub_folder.in_with_stub_stub_folder
 
-# Folders nested
+# -------------------------
+# Folders nested with stubs
+# -------------------------
 
 from stub_folder.with_stub_folder import nested_stub_only, nested_with_stub, \
+    python_only
+
+#? int()
+nested_stub_only.in_stub_only
+#? float()
+nested_with_stub.in_both
+#?
+nested_with_stub.in_python
+#? int()
+nested_with_stub.in_stub
+#? str()
+python_only.in_python
+
+# -------------------------
+# Folders nested with stubs
+# -------------------------
+
+from stub_folder.stub_only_folder import nested_stub_only, nested_with_stub, \
     python_only
 
 #? int()
