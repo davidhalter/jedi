f46d67613018d89fd4d186c37a30513dd5558b71
Dave Halter <davidhalter88@gmail.com>
2019-05-22 00:44:20 +0200

    Fix signature tests

--- test/test_evaluate/test_signature.py
+++ test/test_evaluate/test_signature.py
@@ -1,15 +1,25 @@
 import pytest
+from operator import ge, le
 
 from jedi.evaluate.gradual.conversion import stub_to_actual_context_set
 
 
-def test_compiled_signature(Script):
-    code = 'import math; math.cos'
-    sig = 'cos(x, /)'
+@pytest.mark.parametrize(
+    'code, sig, names, op, version', [
+        ('import math; math.cos', 'cos(x)', ['x'], le, (3, 6)),
+        ('import math; math.cos', 'cos(x, /)', ['x'], ge, (3, 7)),
+
+        ('next', 'next(iterator, default=None)', ['iterator', 'default'], ge, (2, 7)),
+    ]
+)
+def test_compiled_signature(Script, environment, code, sig, names, op, version):
+    if not op(environment.version_info, version):
+        pytest.skip("Not running for this version")
+
     d, = Script(code).goto_definitions()
     context, = d._name.infer()
     compiled, = stub_to_actual_context_set(context)
     signature, = compiled.get_signatures()
     assert signature.to_string() == sig
-    assert signature.get_param_names() == []
+    assert [n.string_name for n in signature.get_param_names()] == names
     assert signature.annotation is None
