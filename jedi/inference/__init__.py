599eded3d105e8fe53b1875415624fece4a135c1
Dave Halter <davidhalter88@gmail.com>
2019-09-05 00:54:13 +0200

    Remove a few unused imports

--- jedi/inference/__init__.py
+++ jedi/inference/__init__.py
@@ -67,18 +67,16 @@ from parso import python_bytes_to_unicode
 from jedi.file_io import FileIO
 
 from jedi import debug
-from jedi import parser_utils
 from jedi.inference import imports
 from jedi.inference import recursion
 from jedi.inference.cache import inference_state_function_cache
 from jedi.inference import helpers
 from jedi.inference.names import TreeNameDefinition
 from jedi.inference.base_value import ContextualizedNode, \
-    ValueSet, NO_VALUES, iterate_values
+    ValueSet, iterate_values
 from jedi.inference.value import ClassValue, FunctionValue
-from jedi.inference.context import CompForContext
 from jedi.inference.syntax_tree import infer_expr_stmt, \
-    infer_node, check_tuple_assignments
+    check_tuple_assignments
 from jedi.inference.imports import follow_error_node_imports_if_possible
 from jedi.plugins import plugin_manager
 
