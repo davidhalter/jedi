4bc4f167e93855a77c3a7264fa40815497dda135
Sam Roeca <samuel.roeca@gmail.com>
2019-11-29 20:11:23 -0500

    Revert "Fix: no longer shows folders recursively to root"
    This reverts commit 03b4177d3dd741df18651e87e0283d55192a6d34.

--- jedi/api/project.py
+++ jedi/api/project.py
@@ -110,10 +110,7 @@ class Project(object):
                 suffixed += discover_buildout_paths(inference_state, inference_state.script_path)
 
                 if add_parent_paths:
-                    traversed = list(traverse_parents(
-                        inference_state.script_path,
-                        root=self._path,
-                    ))
+                    traversed = list(traverse_parents(inference_state.script_path))
 
                     # AFAIK some libraries have imports like `foo.foo.bar`, which
                     # leads to the conclusion to by default prefer longer paths
