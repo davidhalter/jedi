4cc03d223925883bf3d4f1067b9d0e31af0bc36a
Dave Halter <davidhalter88@gmail.com>
2020-02-10 19:51:35 +0100

    Add another rename test

--- test/refactor/rename.py
+++ test/refactor/rename.py
@@ -21,3 +21,24 @@ def test1():
      AssertionError
 -    return test1, test1.not_existing
 +    return blabla, blabla.not_existing
+# ----- different-scopes
+def x():
+    #? 7 v
+    some_var = 3
+    some_var
+def y():
+    some_var = 3
+    some_var
+# +++++
+--- /home/dave/source/jedi/test/refactor/rename.py
++++ /home/dave/source/jedi/test/refactor/rename.py
+@@ -1,7 +1,7 @@
+ def x():
+     #? 7 v
+-    some_var = 3
+-    some_var
++    v = 3
++    v
+ def y():
+     some_var = 3
+     some_var
