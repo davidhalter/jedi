d574162da32052ca378cf08b3429eef2fd11100d
Dave Halter <davidhalter88@gmail.com>
2020-03-10 20:07:10 +0100

    Fix namedtuple docstring/signature issues, fixes #1506

--- jedi/inference/gradual/typeshed.py
+++ jedi/inference/gradual/typeshed.py
@@ -129,6 +129,9 @@ def import_module_decorator(func):
 
 
 def _try_to_load_stub_cached(inference_state, import_names, *args, **kwargs):
+    if import_names is None:
+        return None
+
     try:
         return inference_state.stub_module_cache[import_names]
     except KeyError:
