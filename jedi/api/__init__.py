b4fa42a282b4b2d5dc04d23a1ccdfe4c889567eb
Dave Halter <davidhalter88@gmail.com>
2020-03-13 02:22:05 +0100

    Avoid duplicate definitions for goto, fixes #1514

--- jedi/api/__init__.py
+++ jedi/api/__init__.py
@@ -318,7 +318,8 @@ class Script(object):
         )
 
         defs = [classes.Definition(self._inference_state, d) for d in set(names)]
-        return helpers.sorted_definitions(defs)
+        # Avoid duplicates
+        return list(set(helpers.sorted_definitions(defs)))
 
     @validate_line_column
     def help(self, line=None, column=None):
