5f40fa9bc69ede3ba49d964be87e8d073ce0f54d
Dave Halter <davidhalter88@gmail.com>
2020-03-19 09:48:12 +0100

    Docs: Remove links for sources/created using sphinx/copyright

--- docs/conf.py
+++ docs/conf.py
@@ -167,13 +167,13 @@ html_sidebars = {
 #html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-#html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
