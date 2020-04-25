9b58bf6199f6e7dbc3b2dda417002c4e7307a477
Dave Halter <davidhalter88@gmail.com>
2020-04-25 20:19:13 +0200

    Pin the Django test dependency

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(name='jedi',
               'docopt',
               # coloroma for colored debug output
               'colorama',
-              'Django',
+              'Django==3.0.5',  # For now pin this.
           ],
           'qa': [
               'flake8==3.7.9',
