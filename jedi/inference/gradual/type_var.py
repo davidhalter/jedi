7e637c5e5ee3480da6f8675f04d823f31a40ead3
Peter Law <PeterJCLaw@gmail.com>
2020-06-14 16:27:39 +0100

    Python 2 compatible super()

--- jedi/inference/gradual/type_var.py
+++ jedi/inference/gradual/type_var.py
@@ -124,7 +124,7 @@ class TypeVar(BaseTypingValue):
 
 class TypeWrapper(ValueWrapper):
     def __init__(self, wrapped_value, original_value):
-        super().__init__(wrapped_value)
+        super(TypeWrapper, self).__init__(wrapped_value)
         self._original_value = original_value
 
     def execute_annotation(self):
