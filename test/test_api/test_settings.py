d9a90d5d5e9a74e2ec635a210a60d156202e8e35
Dave Halter <davidhalter88@gmail.com>
2020-07-19 13:55:18 +0200

    Remove a test that no longer made sense

--- test/test_api/test_settings.py
+++ test/test_api/test_settings.py
@@ -1,24 +1,4 @@
-import os
-
-import pytest
-
 from jedi import api
-from jedi.inference import imports
-
-
-@pytest.mark.skipif('True', reason='Skip for now, test case is not really supported.')
-def test_add_dynamic_mods(Script):
-    fname = '__main__.py'
-    api.settings.additional_dynamic_modules = [fname]
-    # Fictional module that defines a function.
-    src1 = "def r(a): return a"
-    # Other fictional modules in another place in the fs.
-    src2 = 'from .. import setup; setup.r(1)'
-    script = Script(src1, path='../setup.py')
-    imports.load_module(script._inference_state, os.path.abspath(fname), src2)
-    result = script.infer()
-    assert len(result) == 1
-    assert result[0].description == 'class int'
 
 
 def test_add_bracket_after_function(monkeypatch, Script):
