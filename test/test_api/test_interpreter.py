f18493b627b55071831d6714f229724f3ece5aec
Dave Halter <davidhalter88@gmail.com>
2020-09-19 20:57:32 +0200

    Fix an interpreter test

--- test/test_api/test_interpreter.py
+++ test/test_api/test_interpreter.py
@@ -621,7 +621,7 @@ def bar():
 
         # typing is available via globals.
         ({'return': 'typing.Union[str, int]'}, ['int', 'str'], ''),
-        ({'return': 'typing.Union["str", int]'}, ['int'], ''),
+        ({'return': 'typing.Union["str", int]'}, ['int', 'str'], ''),
         ({'return': 'typing.Union["str", 1]'}, [], ''),
         ({'return': 'typing.Optional[str]'}, ['NoneType', 'str'], ''),
         ({'return': 'typing.Optional[str, int]'}, [], ''),  # Takes only one arg
