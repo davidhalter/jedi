49e35497ae6246cd26592e14628d06f1cdd59389
Dave Halter <davidhalter88@gmail.com>
2020-10-23 21:28:08 +0200

    Stop subclassing CompiledName, potentially fixes #1667

--- jedi/inference/value/instance.py
+++ jedi/inference/value/instance.py
@@ -401,21 +401,10 @@ class AnonymousInstance(_BaseTreeInstance):
     _arguments = None
 
 
-class CompiledInstanceName(compiled.CompiledName):
-    def __init__(self, inference_state, instance, klass, name):
-        parent_value = klass.parent_context.get_value()
-        assert parent_value is not None, "How? Please reproduce and report"
-        super().__init__(
-            inference_state,
-            parent_value,
-            name.string_name
-        )
-        self._instance = instance
-        self._class_member_name = name
-
+class CompiledInstanceName(NameWrapper):
     @iterator_to_value_set
     def infer(self):
-        for result_value in self._class_member_name.infer():
+        for result_value in self._wrapped_name.infer():
             if result_value.api_type == 'function':
                 yield CompiledBoundMethod(result_value)
             else:
@@ -434,11 +423,7 @@ class CompiledInstanceClassFilter(AbstractFilter):
         return self._convert(self._class_filter.values())
 
     def _convert(self, names):
-        klass = self._class_filter.compiled_value
-        return [
-            CompiledInstanceName(self._instance.inference_state, self._instance, klass, n)
-            for n in names
-        ]
+        return [CompiledInstanceName(n) for n in names]
 
 
 class BoundMethod(FunctionMixin, ValueWrapper):
