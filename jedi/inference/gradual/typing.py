36ea6b32850c852dee87e669613403657b56fe99
Dave Halter <davidhalter88@gmail.com>
2021-01-01 05:19:37 +0100

    Change an import

--- jedi/inference/gradual/typing.py
+++ jedi/inference/gradual/typing.py
@@ -10,14 +10,14 @@ import itertools
 from jedi import debug
 from jedi.inference.compiled import builtin_from_name, create_simple_object
 from jedi.inference.base_value import ValueSet, NO_VALUES, Value, \
-    LazyValueWrapper
+    LazyValueWrapper, ValueWrapper
 from jedi.inference.lazy_value import LazyKnownValues
 from jedi.inference.arguments import repack_with_argument_clinic
 from jedi.inference.filters import FilterWrapper
 from jedi.inference.names import NameWrapper, ValueName
 from jedi.inference.value.klass import ClassMixin
 from jedi.inference.gradual.base import BaseTypingValue, \
-    BaseTypingClassWithGenerics, BaseTypingInstance, ValueWrapper
+    BaseTypingClassWithGenerics, BaseTypingInstance
 from jedi.inference.gradual.type_var import TypeVarClass
 from jedi.inference.gradual.generics import LazyGenericManager, TupleGenericManager
 
