3705af795211c94f9050bfb4435c8ca50da6c5a0
Wouter Vanden Hove <wouter@wvhconsulting.org>
2023-10-13 17:48:37 +0200

    fix typo

--- jedi/settings.py
+++ jedi/settings.py
@@ -86,7 +86,9 @@ cache_directory = os.path.expanduser(_cache_directory)
 The path where the cache is stored.
 
 The cache-directory can be explicitly set by an environment-variable
-``JEDI_CACHE_DIR``. If this env-var in not set, then
+``JEDI_CACHE_DIR``. 
+
+If this env-var is not set, then
 on Linux, the cache-directory defaults to ``~/.cache/jedi/``,
 on OS X to ``~/Library/Caches/Jedi/`` and
 on Windows to ``%LOCALAPPDATA%\\Jedi\\Jedi\\``.
