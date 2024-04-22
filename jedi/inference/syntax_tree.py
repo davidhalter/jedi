f46a4d8963e1efb77973997b9d3d1f405e5c529e
pyscripter <pyscripter@gmail.com>
2024-04-22 16:47:17 +0300

    Added test to test/completion/pep0484_basic.py Addressed feedback from Dave

--- jedi/inference/syntax_tree.py
+++ jedi/inference/syntax_tree.py
@@ -696,11 +696,11 @@ def tree_name_to_values(inference_state, context, tree_name):
             if expr_stmt.type == "expr_stmt" and expr_stmt.children[1].type == "annassign":
                 correct_scope = parser_utils.get_parent_scope(name) == context.tree_node
                 ann_assign = expr_stmt.children[1]
-                if correct_scope and name.start_pos < ann_assign.start_pos:
+                if correct_scope:
                     found_annotation = True
                     if (
-                        (expr_stmt.children[1].children[1].type == 'name')
-                        and (expr_stmt.children[1].children[1].value == tree_name.value)
+                        (ann_assign.children[1].type == 'name')
+                        and (ann_assign.children[1].value == tree_name.value)
                         and context.parent_context
                     ):
                         context = context.parent_context
