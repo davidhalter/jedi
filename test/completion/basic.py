8e4df5cc0ec511db1af6d358182b1fb7c1e0cbff
Dave Halter <davidhalter88@gmail.com>
2026-05-02 01:08:27 +0200

    Make sure the new generic syntax does not fail with latest parso

--- test/completion/basic.py
+++ test/completion/basic.py
@@ -436,3 +436,16 @@ res = 'f' in 'foo'; res
 
 #? bool()
 res = not {}; res
+
+# -----------------
+# new generic syntax should not fail
+# -----------------
+
+class C[T]:
+    def c(self) -> str: ...
+def f[T](x: T, y: T) -> int: ...
+
+#? int()
+f()
+#? str()
+C().c()
